e787ef7a115431f9ec520cf01054057acee3301f
DmitriyALEX <doiroffical@gmail.com>
2023-07-09 14:59:10 +0300

    Task 7

--- index.js
+++ index.js
@@ -12,52 +12,93 @@
 
 
 //4. Mathematical operations 
-const mathAction = () => {
-   const condition1 = Number(prompt("Enter First Number"))
-   const condition2 = Number(prompt("Enter Second Number"))
-   alert(`
-      ${condition1} + ${condition2} = ${condition1 + condition2}
-      ${condition1} - ${condition2} = ${condition1 - condition2}
-      ${condition1} * ${condition2} = ${condition1 * condition2}
-      ${condition1} / ${condition2} = ${condition1 / condition2}
-   `)
-   }
-mathAction()
+// const mathAction = () => {
+//    const condition1 = Number(prompt("Enter First Number"))
+//    const condition2 = Number(prompt("Enter Second Number"))
+//    alert(`
+//       ${condition1} + ${condition2} = ${condition1 + condition2}
+//       ${condition1} - ${condition2} = ${condition1 - condition2}
+//       ${condition1} * ${condition2} = ${condition1 * condition2}
+//       ${condition1} / ${condition2} = ${condition1 / condition2}
+//    `)
+//    }
+// mathAction()
 
 
 //5. Average value (Середнє значення)
-const averageValue = () => {
-   const value1 = Number(prompt("Enter First Number"))
-   const value2 = Number(prompt("Enter Second Number"))
-   const value3 = Number(prompt("Enter third Number"))
-   alert((value1 + value2 + value3) / 3)
-}
-averageValue()
+// const averageValue = () => {
+//    const value1 = Number(prompt("Enter First Number"))
+//    const value2 = Number(prompt("Enter Second Number"))
+//    const value3 = Number(prompt("Enter third Number"))
+//    alert((value1 + value2 + value3) / 3)
+// }
+// averageValue()
 
 
 // 6. Calculator (Kалькулятор) 
-const calculator = () => {
-   const userAction = prompt("Enter, what do you want: add, sub, mult, div")
-   const firstValue =  Number(prompt("Enter First Number"))
-   const secondValue = Number(prompt("Enter Second Number"))
+// const calculator = () => {
+//    const userAction = prompt("Enter, what do you want: add, sub, mult, div")
+//    const firstValue =  Number(prompt("Enter First Number"))
+//    const secondValue = Number(prompt("Enter Second Number"))
 
-   if (userAction === "add") {
-      const addValue = firstValue + secondValue
-      alert(`${firstValue} + ${secondValue} = ${addValue}`)
-   } 
-   if (userAction === "sub") {
-      const subValue = firstValue - secondValue
-      alert(`${firstValue} - ${secondValue} = ${subValue}`)
-   } 
-   if (userAction === "mult") {
-      const multValue = firstValue * secondValue
-      alert(`${firstValue} * ${secondValue} = ${multValue}`)
-   }
-   if (userAction === "div") {
-      const divValue = firstValue / secondValue
-      alert(`${firstValue} / ${secondValue} = ${divValue}`)
-   }   
+//    if (userAction === "add") {
+//       const addValue = firstValue + secondValue
+//       alert(`${firstValue} + ${secondValue} = ${addValue}`)
+//    } 
+//    if (userAction === "sub") {
+//       const subValue = firstValue - secondValue
+//       alert(`${firstValue} - ${secondValue} = ${subValue}`)
+//    } 
+//    if (userAction === "mult") {
+//       const multValue = firstValue * secondValue
+//       alert(`${firstValue} * ${secondValue} = ${multValue}`)
+//    }
+//    if (userAction === "div") {
+//       const divValue = firstValue / secondValue
+//       alert(`${firstValue} / ${secondValue} = ${divValue}`)
+//    }   
+// }
+// calculator()
+
+
+// 7. Numbers and strings (Числа та рядки)
+const numberString = () => {
+   const string1 = prompt("Enter your string")
+   const string2 = prompt("Enter your string")
+   const string3 = prompt("Enter your string")
+
+   alert(`${string1 + string2 + string3}`)
 }
-calculator()
+numberString()
+
+// 7.1  Five Numbers (вивести п'ятизначне число з пробілами)
+function fiveNumbers () {
+   const numbers = prompt('Enter five numbers')
+   const arr = numbers.split('')
+   alert(arr.join(' '))
+}
+fiveNumbers()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
